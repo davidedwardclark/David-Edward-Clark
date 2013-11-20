f6e0e6b88e223a7971f271ce42e2e00ca47ab950
David Clark <davidedwardclark@gmail.com>
2013-11-20 06:59:34 -0800

    removed deploy file because S3

--- deploy/production.sh
+++
@@ -1,1 +0,0 @@
-rsync -arvuz /Users/davidedwardclark/Projects/David-Edward-Clark/ root@173.255.210.11:/srv/www/davidedwardclark.com/public_html/ --rsh="ssh -p25386" --exclude '.git' --exclude '.DS_Store' --exclude '/deploy/production.sh';
